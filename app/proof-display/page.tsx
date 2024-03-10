9f2fa2d837b7c17e55fd1a3a28b8d756fbda383e
kzoeps <kzoepa@gmail.com>
2024-03-10 14:55:14 +0900

    ta

--- app/proof-display/page.tsx
+++ app/proof-display/page.tsx
@@ -1,21 +1,19 @@
-export default function ProofDisplay({params, searchParams}: any) {
+import { ChakraProvider } from "@chakra-ui/react"
+import Success from "../_components/proof-success"
+import Nav from "../_components/nav"
+
+export default function ProofDisplay({ params, searchParams }: any) {
     const mappedParams = Object.entries(searchParams).map(([key, value]) => {
         return { display: key, value }
     })
     console.log(mappedParams)
     return (
         <>
-            <div>
-                Here is the proof that you requested and its details are as follows
-            </div>
-            <ol>
-                {mappedParams.map(({ display, value }: any) => (
-                    <li key={value}>
-                        <strong>{display}</strong>: {value}
-                    </li>
-                ))}
-            </ol>
+            <ChakraProvider>
+                <Nav />
+                <Success params={mappedParams}/>
+            </ChakraProvider>
 
-            </>
+        </>
     )
 }
