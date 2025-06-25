bb4d0c2af8ccba1daaca089fb4af02cf8ba2afa1
kzoeps <kzoepa@gmail.com>
2025-06-25 23:13:47 +0600

    feat: update callback url

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,6 +1,7 @@
 import { NextApiResponse } from "next";
 
 export async function POST(request: Request, response: NextApiResponse) {
-    const body = await request.json();
-    return new Response('Credential Verified')
+  const body = await request.json();
+  console.log("Payload: ", body);
+  return new Response("Credential Verified");
 }
