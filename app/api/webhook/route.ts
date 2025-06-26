dd824264c4ac98e15a507fba92baedb0ed0e5221
kzoeps <kzoepa@gmail.com>
2025-06-26 15:11:04 +0600

    feat: udpate route

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -2,6 +2,6 @@ import { NextApiResponse } from "next";
 
 export async function POST(request: Request, response: NextApiResponse) {
   const body = await request.json();
-  console.log("Payload: ", body);
+  console.log(body)
   return new Response("Credential Verified");
 }
